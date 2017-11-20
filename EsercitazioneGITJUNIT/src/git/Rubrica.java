2dec38792cced53ab5b153708d2785734e6bfbf3
EnricoD93 <32543315+EnricoD93@users.noreply.github.com>
2017-11-20 14:06:27 +0100

    Rubrica created

---
+++ EsercitazioneGITJUNIT/src/git/Rubrica.java
@@ -0,0 +1,35 @@
+package git;
+
+import java.util.ArrayList;
+
+import javax.xml.crypto.dsig.spec.ExcC14NParameterSpec;
+
+public class Rubrica {
+	ArrayList<String> contatti;
+
+	public Rubrica() {
+		contatti = new ArrayList<>();
+	}
+
+	void aggiungiContatto(String s) {
+		contatti.add(s);
+	}
+
+	String prendiContatto(int i) {
+		if (i < contatti.size())
+			return contatti.get(i);
+		return null;
+	}
+
+	void rimuoviContatto(String s) {
+		if (contatti.contains(s))
+			contatti.remove(s);
+		else
+			try {
+				throw new Exception("Non trovato");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+}
